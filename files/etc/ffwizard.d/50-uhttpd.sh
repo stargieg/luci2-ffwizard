0ae06c227eaab3cecda8c7b9e72e2dd11333828d
Patrick <patrick@lunatiki.de>
2015-03-08 19:38:21 +0100

    use uci.sh functions

--- files/etc/ffwizard.d/50-uhttpd.sh
+++ files/etc/ffwizard.d/50-uhttpd.sh
@@ -3,20 +3,20 @@
 #remove https certs
 #set new common name
 #generate new certs with uhttpd restart
-hostname=$(uci get system.@system[0].hostname)
+hostname=$(uci_get system.@system[0].hostname)
 config_load uhttpd
 config_get crtfile main cert
 config_get keyfile main key
 rm -f $crtfile
 rm -f $keyfile
-uci set uhttpd.px5g.commonname=$hostname
-uci commit
+uci_set uhttpd px5g commonname $hostname
+uci_commit uhttpd
 
 
 #set ports for list on ipv6
-uci set uhttpd.main.listen_http=80
-uci set uhttpd.main.listen_https=443
-uci commit
+uci_set uhttpd main listen_http 80
+uci_set uhttpd main listen_https 443
+uci_commit
 
 #restart
 /etc/init.d/uhttpd restart
