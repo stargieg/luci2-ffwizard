362336cd2cf7d090d3da192d4a3bc7e4ca05118d
Patrick Grimm <patrick@lunatiki.de>
2015-03-11 20:53:09 +0100

    rewrite batman bridge support

--- files/etc/ffwizard.d/30-batadv.sh
+++ files/etc/ffwizard.d/30-batadv.sh
@@ -1,16 +1,17 @@
 
-setup_batbase() {
+setup_bat_base() {
 	local cfg=$1
 	uci_add batman-adv mesh $cfg
 	uci_set batman-adv $cfg aggregated_ogms
 	uci_set batman-adv $cfg ap_isolation
 	uci_set batman-adv $cfg bonding
 	uci_set batman-adv $cfg fragmentation
-	uci_set batman-adv $cfg gw_bandwidth
-	#uci_set batman-adv $cfg gw_bandwidth '50mbit/50mbit'
-	#TODO Select Server/Client
+	#TODO Select Client
 	uci_set batman-adv $cfg gw_mode 'client'
+	uci_set batman-adv $cfg gw_bandwidth
+	#TODO Select Server
 	#uci_set batman-adv $cfg gw_mode 'server'
+	#uci_set batman-adv $cfg gw_bandwidth '50mbit/50mbit'
 	uci_set batman-adv $cfg gw_sel_class
 	uci_set batman-adv $cfg log_level
 	uci_set batman-adv $cfg orig_interval
@@ -21,45 +22,30 @@ setup_batbase() {
 	uci_set batman-adv $cfg hop_penalty
 }
 
-setup_bridge_bat() {
-	local cfg=$1
-	local ifc=$2
-	cfg_iface=$(uci_get network $cfg ifname
-	uci_set network $cfg ifname "$cfg_iface $ifc"
-}
-
-
 setup_ether() {
 	local cfg=$1
-	local br=$2
-	local bat_ifc=$3
+	local bat_ifc=$2
 	config_get enabled $cfg enabled "0"
 	[ "$enabled" == "0" ] && return
 	config_get device $cfg device "0"
 	[ "$device" == "0" ] && return
 	config_get bat_mesh $cfg bat_mesh "0"
-	if [ -n $br ]; then
-		logger -t "ffwizard_bat_ether_br" "Setup $cfg"
-		ifname=$(uci_get network $cfg ifname)
-		bat_ifaces="$bat_ifaces $ifname"
-		uci_set network $cfg proto none
-		bat_enabled=1
-	elif [ $bat_mesh == "1" ]; then
-		logger -t "ffwizard_bat_ether_mesh" "Setup $cfg"
-		uci_set network $cfg proto batadv
-		uci_set network $cfg mesh "$bat_ifc"
-		bat_enabled=1
-	fi
+	[ "$bat_mesh" == "0" ] && return
+	logger -t "ffwizard_bat_ether_mesh" "Setup $cfg"
+	uci_set network $cfg proto batadv
+	uci_set network $cfg mesh "$bat_ifc"
+	bat_enabled=1
 }
 
 setup_wifi() {
 	local cfg=$1
+	local bat_ifc=$2
 	config_get enabled $cfg enabled "0"
 	[ "$enabled" == "0" ] && return
 	config_get device $cfg device "0"
 	[ "$device" == "0" ] && return
 	config_get bat_mesh $cfg bat_mesh "0"
-	[ "$device" == "0" ] && return
+	[ "$bat_mesh" == "0" ] && return
 	logger -t "ffwizard_bat_wifi" "Setup $cfg"
 	uci_set network $cfg proto batadv
 	uci_set network $cfg mesh "$bat_ifc"
@@ -79,15 +65,13 @@ config_foreach remove_section mesh
 local bat_enabled=0
 #Setup ether and wifi
 config_load ffwizard
-config_foreach setup_iface ether fflandhcp bat0
-config_foreach setup_wifi wifi
-
-setup_bridge_bat fflandhcp "$bat_ifaces"
+config_foreach setup_iface ether bat0
+config_foreach setup_wifi wifi bat0
 
 if [ $bat_enabled == "1" ] ; then
 	#Setup batman-adv
 	config_load batman-adv
-	setup_batbase bat0
+	setup_bat_base bat0
 	uci_commit batman-adv
 	uci_commit network
 	#/etc/init.d/batman enable
