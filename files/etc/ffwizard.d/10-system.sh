090d5f5d48065cab281ccc9c702cc345bfefd79f
Patrick Grimm <patrick@lunatiki.de>
2015-04-03 00:22:10 +0200

    use uci function

--- files/etc/ffwizard.d/10-system.sh
+++ files/etc/ffwizard.d/10-system.sh
@@ -1,36 +1,50 @@
 
+setup_system() {
+	local cfg=$1
+	uci_set system $cfg hostname "$hostname"
+	echo $hostname > /proc/sys/kernel/hostname
+
+	# Set Timezone
+	uci_set system $cfg zonename "Europe/Berlin"
+	uci_set system $cfg timezone "CET-1CEST,M3.5.0,M10.5.0/3"
+
+	# Set Location
+	if [ -n "$location" ] ; then
+		uci_set system $cfg location "$location"
+	fi
+	# Set Geo Location
+	if [ -n "$latitude" ] ; then
+		uci_set system $cfg latitude "$latitude"
+	fi
+	if [ -n "$longitude" ] ; then
+		uci_set system $cfg longitude "$longitude"
+	fi
+}
+
+#Load ffwizard config
 config_load ffwizard
-config_get hostname ffwizard hostname
-config_get location ffwizard location
-config_get latitude ffwizard latitude
-config_get longitude ffwizard longitude
 
 # Set Hostname
-if [ -z $hostname ] ; then
+config_get hostname ffwizard hostname
+if [ -z "$hostname" ] ; then
 	rand="$(echo -n $(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1-4))"
 	rand="$(printf "%d" "0x$rand")"
 	hostname="OpenWrt-$rand"
 	uci_set ffwizard ffwizard hostname "$hostname"
 fi
 
-uci_set system @system[0] hostname "$hostname"
-echo $hostname > /proc/sys/kernel/hostname
+# Set lat lon
+config_get location ffwizard location
+config_get latitude ffwizard latitude
+config_get longitude ffwizard longitude
 
-# Set Timezone
-uci_set system @system[0] zonename "Europe/Berlin"
-uci_set system @system[0] timezone "CET-1CEST,M3.5.0,M10.5.0/3"
 
-# Set Location
-if [ -n $location ] ; then
-	uci_set system @system[0] location $location
-fi
-# Set Geo Location
-if [ -n $latitude ] ; then
-	uci_set system @system[0] latitude $latitude
-fi
-if [ -n $longitude ] ; then
-	uci set system @system[0] longitude $longitude
-fi
+#Load dhcp config
+config_load system
+#Setup dnsmasq
+config_foreach setup_system system
+
+
 
 uci_commit ffwizard
 uci_commit system
