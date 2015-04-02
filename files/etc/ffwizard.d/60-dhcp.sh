40212dca6e3decef137ee4b9dd208c64aaf00952
Patrick Grimm <patrick@lunatiki.de>
2015-04-03 01:26:15 +0200

    escape vars fix typos

--- files/etc/ffwizard.d/60-dhcp.sh
+++ files/etc/ffwizard.d/60-dhcp.sh
@@ -1,7 +1,8 @@
 
+
 setup_dhcp() {
-		local cfg_dhcp=$1
-		local ipaddr=$2
+		local cfg_dhcp="$1"
+		local ipaddr="$2"
 		if uci_get dhcp $cfg_dhcp >/dev/null ; then
 			uci_remove dhcp $cfg_dhcp
 		fi
@@ -11,51 +12,51 @@ setup_dhcp() {
 		OCTET_4="$((OCTET_4 + 2))"
 		ipaddr="$OCTET_1_3.$OCTET_4"
 		uci_add dhcp dhcp $cfg_dhcp
-		uci_set dhcp $cfg_dhcp interface $cfg_dhcp
-		uci_set dhcp $cfg_dhcp start $ipaddr
+		uci_set dhcp $cfg_dhcp interface "$cfg_dhcp"
+		uci_set dhcp $cfg_dhcp start "$ipaddr"
 		limit=$(($((2**$((32-$PREFIX))))-2))
-		uci_set dhcp $cfg_dhcp limit $limit
-		uci_set dhcp $cfg_dhcp leasetime '15m'
-		#uci_set dhcp $cfg_dhcp list dhcp_option '119,olsr'
+		uci_set dhcp $cfg_dhcp limit "$limit"
+		uci_set dhcp $cfg_dhcp leasetime "15m"
+		#uci_set dhcp $cfg_dhcp list dhcp_option "119,olsr"
 		/sbin/uci add_list dhcp.$cfg_dhcp.dhcp_option="119,olsr"
 		/sbin/uci add_list dhcp.$cfg_dhcp.dhcp_option="119,lan"
-		uci_set dhcp $cfg_dhcp ra server
-		uci_set dhcp $cfg_dhcp dhcpv6 server
+		uci_set dhcp $cfg_dhcp ra "server"
+		uci_set dhcp $cfg_dhcp dhcpv6 "server"
 }
 
 setup_iface() {
-	local cfg=$1
+	local cfg="$1"
 	config_get enabled $cfg enabled "0"
 	[ "$enabled" == "0" ] && return
 	config_get dhcp_ip $cfg dhcp_ip "0"
 	if [ "$dhcp_ip" != "0" ] ; then
 		logger -t "ffwizard_dhcp_iface" "Setup $cfg"
 		cfg_dhcp=$cfg"_dhcp"
-		setup_dhcp $cfg_dhcp $ipaddr
+		setup_dhcp $cfg_dhcp "$ipaddr"
 	fi
 }
 
 setup_wifi() {
-	local cfg=$1
+	local cfg="$1"
 	config_get enabled $cfg enabled "0"
 	[ "$enabled" == "0" ] && return
 	config_get dhcp_ip $cfg dhcp_ip "0"
 	if [ "$dhcp_ip" != "0" ] ; then
 		logger -t "ffwizard_dhcp_wifi" "Setup $cfg"
-		cfg_dhcp=$cfg"_dhcp"
-		setup_dhcp $cfg_dhcp $ipaddr
+		cfg_dhcp=$cfg"_vap"
+		setup_dhcp $cfg_dhcp "$dhcp_ip"
 	fi
 }
 
 setup_dhcpbase() {
-	local cfg=$1
-	uci_set dhcp $cfg local '/olsr/'
-	uci_set dhcp $cfg domain 'olsr'
+	local cfg="$1"
+	uci_set dhcp $cfg local "/olsr/"
+	uci_set dhcp $cfg domain "olsr"
 }
 
 setup_odhcpbase() {
-	local cfg=$1
-	uci_set dhcp $cfg maindhcp 1
+	local cfg="$1"
+	uci_set dhcp $cfg maindhcp "1"
 }
 
 local br_name="fflandhcp"
