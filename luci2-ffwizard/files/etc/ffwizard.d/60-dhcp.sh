8b34c8ac52db13325e9a2fa40e4832f5f641879c
Patrick Grimm <patrick@lunatiki.de>
2015-12-07 03:33:24 +0100

    add standard dhcp config for non mesh interfaces

--- luci2-ffwizard/files/etc/ffwizard.d/60-dhcp.sh
+++ luci2-ffwizard/files/etc/ffwizard.d/60-dhcp.sh
@@ -1,4 +1,13 @@
 
+uci_add_list() {
+	local PACKAGE="$1"
+	local CONFIG="$2"
+	local OPTION="$3"
+	local VALUE="$4"
+
+	/sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} add_list "$PACKAGE.$CONFIG.$OPTION=$VALUE"
+}
+
 log_dhcp() {
 	logger -s -t ffwizard_dhcp $@
 }
@@ -11,20 +20,22 @@ setup_dhcp() {
 		fi
 		uci_add dhcp dhcp $cfg_dhcp
 		uci_set dhcp $cfg_dhcp interface "$cfg_dhcp"
+		uci_set dhcp $cfg_dhcp ignore "0"
 		if [ -n "$ipaddr" ] ; then
 			eval "$(ipcalc.sh $ipaddr)"
 			OCTET_4="${NETWORK##*.}"
 			OCTET_1_3="${NETWORK%.*}"
 			OCTET_4="$((OCTET_4 + 2))"
-			ipaddr="$OCTET_1_3.$OCTET_4"
-			uci_set dhcp $cfg_dhcp start "$ipaddr"
+			start_ipaddr="$OCTET_4"
+			uci_set dhcp $cfg_dhcp start "$start_ipaddr"
 			limit=$(($((2**$((32-$PREFIX))))-2))
 			uci_set dhcp $cfg_dhcp limit "$limit"
+		else
+			uci_set dhcp $cfg_dhcp dhcpv4 "disabled"
 		fi
 		uci_set dhcp $cfg_dhcp leasetime "15m"
-		#uci_set dhcp $cfg_dhcp list dhcp_option "119,olsr"
-		/sbin/uci add_list dhcp.$cfg_dhcp.dhcp_option="119,olsr"
-		/sbin/uci add_list dhcp.$cfg_dhcp.dhcp_option="119,lan"
+		uci_add_list dhcp $cfg_dhcp dhcp_option "119,olsr"
+		uci_add_list dhcp $cfg_dhcp dhcp_option "119,lan"
 		uci_set dhcp $cfg_dhcp dhcpv6 "server"
 		uci_set dhcp $cfg_dhcp ra "server"
 		uci_set dhcp $cfg_dhcp ra_preference "low"
@@ -36,10 +47,30 @@ setup_ether() {
 	[ "$enabled" == "0" ] && return
 	config_get dhcp_ip $cfg dhcp_ip "0"
 	if [ "$dhcp_ip" != "0" ] ; then
-		logger -t "ffwizard_dhcp_ether" "Setup $cfg"
+		log_dhcp "Setup $cfg"
 		cfg_dhcp=$cfg"_dhcp"
-		setup_dhcp $cfg_dhcp "$ipaddr"
+		setup_dhcp $cfg_dhcp "$dhcp_ip"
+	else
+		config_get dhcp_br $cfg dhcp_br "0"
+		config_get mesh_ip $cfg mesh_ip "0"
+		if [ "$cfg" == "lan" ] && [ "$mesh_ip" == "0" ] && [ "$dhcp_br" == "0" ] ; then
+			log_dhcp "Setup iface $cfg to default"
+			uci_set dhcp $cfg ignore "0"
+			uci_set dhcp $cfg dhcpv4 "server"
+			uci_set dhcp $cfg dhcpv6 "server"
+			uci_set dhcp $cfg ra "server"
+			uci_set dhcp $cfg_dhcp ra_preference "low"
+		else
+			uci_set dhcp $cfg ignore "1"
+			uci_set dhcp $cfg dhcpv4 "disabled"
+			uci_set dhcp $cfg dhcpv6 "disabled"
+			uci_set dhcp $cfg ra "disabled"
+		fi
 	fi
+	case $cfg in
+		lan) lan_iface="";;
+		wan) wan_iface="";;
+	esac
 }
 
 setup_wifi() {
@@ -47,10 +78,15 @@ setup_wifi() {
 	config_get enabled $cfg enabled "0"
 	[ "$enabled" == "0" ] && return
 	config_get dhcp_ip $cfg dhcp_ip "0"
+	cfg_dhcp=$cfg"_vap"
 	if [ "$dhcp_ip" != "0" ] ; then
-		logger -t "ffwizard_dhcp_wifi" "Setup $cfg"
-		cfg_dhcp=$cfg"_vap"
+		log_dhcp "Setup $cfg with $dhcp_ip"
 		setup_dhcp $cfg_dhcp "$dhcp_ip"
+	else
+		if uci_get dhcp $cfg_dhcp >/dev/null ; then
+			log_dhcp "Setup $cfg remove"
+			uci_remove dhcp $cfg_dhcp 2>/dev/null
+		fi
 	fi
 }
 
@@ -66,6 +102,8 @@ setup_odhcpbase() {
 }
 
 local br_name="fflandhcp"
+local lan_iface="lan"
+local wan_iface="wan"
 #Load dhcp config
 config_load dhcp
 #Setup dnsmasq
@@ -77,14 +115,40 @@ config_foreach setup_odhcpbase odhcpd
 #Setup ether and wifi
 config_load ffwizard
 config_foreach setup_ether ether
-config_foreach setup_wifi wifi $br_name
+config_foreach setup_wifi wifi
 
 #Setup DHCP Batman Bridge
 config_get br ffwizard br "0"
 if [ "$br" == "1" ] ; then
-	log_dhcp "Setup iface $br_name"
-	config_get ipaddr ffwizard dhcp_ip
-	setup_dhcp $br_name $ipaddr
+	config_get dhcp_ip ffwizard dhcp_ip
+	log_dhcp "Setup iface $br_name with ip $dhcp_ip"
+	setup_dhcp $br_name $dhcp_ip
+else
+	if uci_get dhcp $br_name >/dev/null ; then
+		log_dhcp "Setup $br_name remove"
+		uci_remove dhcp $br_name 2>/dev/null
+	fi
+fi
+
+
+#Enable dhcp on LAN
+if [ -n "$lan_iface" ] ; then
+	log_dhcp "Setup iface $lan_iface to default"
+	uci_set dhcp $lan_iface start "100"
+	uci_set dhcp $lan_iface limit "150"
+	uci_set dhcp $lan_iface leasetime "12h"
+	uci_set dhcp $lan_iface dhcpv4 "server"
+	uci_set dhcp $lan_iface dhcpv6 "server"
+	uci_set dhcp $lan_iface ra "server"
+fi
+
+#Disable dhcp on WAN
+if [ -n "$wan_iface" ] ; then
+	log_dhcp "Setup iface $lan_iface to default"
+	uci_set dhcp $wan_iface ignore "1"
+	uci_set dhcp $cfg dhcpv4 "disabled"
+	uci_set dhcp $cfg dhcpv6 "disabled"
+	uci_set dhcp $cfg ra "disabled"
 fi
 
 uci_commit dhcp
