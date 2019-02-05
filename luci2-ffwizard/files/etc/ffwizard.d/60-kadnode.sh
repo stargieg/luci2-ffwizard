be88a840780587ff6e66776e4fa254716bf55f7e
Patrick Grimm <patrick@lunatiki.de>
2019-02-05 15:49:19 +0100

    ffwizard: update kadnode
    Signed-off-by: Patrick Grimm <patrick@lunatiki.de>

--- luci2-ffwizard/files/etc/ffwizard.d/60-kadnode.sh
+++ luci2-ffwizard/files/etc/ffwizard.d/60-kadnode.sh
@@ -1,4 +1,6 @@
 
+iface=""
+
 uci_add_list() {
 	local PACKAGE="$1"
 	local CONFIG="$2"
@@ -12,11 +14,49 @@ log_kadnode() {
 	logger -s -t ffwizard_kadnode $@
 }
 
+setup_ether() {
+	local cfg="$1"
+	config_get enabled $cfg enabled "0"
+	[ "$enabled" == "0" ] && return
+	config_get olsr_mesh $cfg olsr_mesh "0"
+	if [ "$olsr_mesh" == "1" ] ; then
+		iface=$cfg
+	fi
+}
+
+setup_wifi() {
+	local cfg="$1"
+	config_get enabled $cfg enabled "0"
+	[ "$enabled" == "0" ] && return
+	config_get olsr_mesh $cfg olsr_mesh "0"
+	if [ "$olsr_mesh" == "1" ] ; then
+		iface=$cfg"_mesh"
+	fi
+}
+
 setup_kadnode() {
 	local cfg="$1"
 	local hostname="$2"
-	uci_remove kadnode $cfg value_id
-	uci_add_list kadnode $cfg value_id "$hostname"
+	local iface="$3"
+	uci_set kadnode $cfg enabled "1"
+	uci_set kadnode $cfg ipv6 "1"
+	uci_set kadnode $cfg ipv4 "0"
+	uci_set kadnode $cfg verbosity "quiet"
+	uci_set kadnode $cfg port "5881"
+	uci_remove kadnode $cfg peer 2>/dev/null
+	uci_set kadnode $cfg peerfile '/etc/kadnode_peers.txt'
+	#uci_set ifname $cfg "br-lan"
+	uci_set kadnode $cfg ifname "$iface"
+	uci_remove kadnode $cfg bob_load_key 2>/dev/null
+	uci_add_list kadnode $cfg bob_load_key '/etc/kadnode_secret.pem'
+	uci_remove kadnode $cfg tls_server_cert 2>/dev/null
+	uci_add_list kadnode $cfg tls_server_cert '/etc/uhttpd.crt,/etc/uhttpd.key'
+	uci_remove kadnode $cfg tls_client_cert 2>/dev/null
+	uci_add_list kadnode $cfg tls_client_cert '/etc/ssl/cert.pem'
+	uci_remove kadnode $cfg announce 2>/dev/null
+	uci_add_list kadnode $cfg announce "$hostname"".olsr"
+	local hostid="$(rm /etc/kadnode_secret.pem;kadnode --bob-create-key /etc/kadnode_secret.pem | grep 'Public key' | cut -d ' ' -f 3)"
+	uci_add_list kadnode $cfg announce "$hostid"
 }
 
 #Load ffwizard config
@@ -25,9 +65,13 @@ config_load ffwizard
 # Set Hostname
 config_get hostname ffwizard hostname "OpenWrt"
 
+#Setup ether and wifi
+config_foreach setup_ether ether
+config_foreach setup_wifi wifi
+
 #Load kadnode config
 config_load kadnode
 #Setup kadnode
-config_foreach setup_kadnode kadnode "$hostname"
+config_foreach setup_kadnode kadnode "$hostname" "$iface"
 
 uci_commit kadnode
