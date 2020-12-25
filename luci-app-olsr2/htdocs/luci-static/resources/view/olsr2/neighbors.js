1935b5255dd1a407ba710c5bb4f4b5cc70c5cba1
Patrick Grimm <patrick@lunatiki.de>
2020-12-25 14:23:15 +0100

    add tabmenu for olsr public stats

--- luci-app-olsr2/htdocs/luci-static/resources/view/olsr2/neighbors.js
+++ luci-app-olsr2/htdocs/luci-static/resources/view/olsr2/neighbors.js
@@ -28,8 +28,16 @@ return view.extend({
 				);
 		}
 			
-		var table = E('div', { 'class': 'table' });
+		var table = E('div',{ 'class': 'olsr'});
+		table.appendChild(E('ul', { 'class': 'tabs' }, [
+			E('li', { 'class': 'tabmenu-item-admin' }, [ E('a',{ 'href': '/cgi-bin/luci/admin'}, _(' Admin')) ]),
+			E('li', { 'class': 'tabmenu-item-overview' }, [ E('a',{ 'href': '../olsr2'}, _('Overview')) ]),
+			E('li', { 'class': 'tabmenu-item-node' }, [ E('a',{ 'href': '../olsr2/node'}, _('Node')) ]),
+			E('li', { 'class': 'tabmenu-item-attachednetwork' }, [ E('a',{ 'href': '../olsr2/attachednetwork'}, _('Attachednetwork')) ]),
+			E('li', { 'class': 'tabmenu-item-neighbors' }, [ E('a',{ 'href': '../olsr2/neighbors'}, _('Neighbors')) ]),
+		]));
 
+		table.appendChild(E('div', { 'class': 'table' }));
 		for (var i = 0; i < fields.length; i += 3) {
 			table.appendChild(E('div', { 'class': 'tr' }, [
 				E('div', { 'class': 'td left', 'width': '33%' }, [ fields[i] ]),
