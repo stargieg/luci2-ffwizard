c511bbf99befb98192c0ef524bd1b7ad5dd78aad
Patrick Grimm <patrick@lunatiki.de>
2022-09-08 22:33:29 +0200

    luci2-ffwizard: move olsr2hosts from lua to shell

---
+++ luci2-ffwizard/files/usr/sbin/olsr2hosts.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+. /lib/functions.sh
+. /usr/share/libubox/jshn.sh
+
+log() {
+	logger -s -t olsr2hosts $@
+}
+
+json_init
+json_load "$(echo '/nhdpinfo json neighbor /quit' | nc ::1 2009)"
+if ! json_select neighbor ; then
+	log "Exit no lan entry"
+	return 1
+fi
+
+i=1;while json_is_a ${i} object;do
+	json_select ${i}
+	json_get_var neighborip neighbor_originator
+	neighborname=$(nslookup $neighborip $neighborip | grep 'name =' | cut -d ' ' -f 3)
+	neighborips=$(nslookup $neighborname $neighborip | grep 'Address.*: [1-9a-f][0-9a-f]\{0,3\}:' | cut -d ' ' -f 3)
+	for j in $neighborips ; do
+		echo "$j $neighborname"
+	done
+	json_select ..
+	i=$(( i + 1 ))
+done
