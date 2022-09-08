c27b6204a75780b9a4ba1949c64f5393ea8bc511
Patrick Grimm <patrick@lunatiki.de>
2022-09-08 23:02:11 +0200

    luci2-ffwizard: remove olsr domain

--- luci2-ffwizard/files/usr/sbin/olsr2hosts.sh
+++ luci2-ffwizard/files/usr/sbin/olsr2hosts.sh
@@ -17,7 +17,7 @@ fi
 i=1;while json_is_a ${i} object;do
 	json_select ${i}
 	json_get_var neighborip neighbor_originator
-	neighborname=$(nslookup $neighborip $neighborip | grep 'name =' | cut -d ' ' -f 3)
+	neighborname=$(nslookup $neighborip $neighborip | grep 'name =' | cut -d ' ' -f 3 | cut -d '.' -f -1)
 	neighborips=$(nslookup $neighborname $neighborip | grep 'Address.*: [1-9a-f][0-9a-f]\{0,3\}:' | cut -d ' ' -f 2)
 	for j in $neighborips ; do
 		echo "$j $neighborname"
