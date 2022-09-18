5fd96c5e88500e67a691e509e779962fd8a1df07
Patrick Grimm <patrick@lunatiki.de>
2022-09-18 02:28:00 +0200

    luci-mod-freifunk: sync with https://github.com/freifunk/openwrt-packages

--- luci-app-olsr-public/luasrc/controller/olsr_public.lua
+++ luci-app-olsr-public/luasrc/controller/olsr_public.lua
@@ -6,6 +6,13 @@ module("luci.controller.olsr_public", package.seeall)
 function index()
 	local page
 
+	page          = node("public")
+	page.title    = _("Public")
+	page.target   = alias("public", "status")
+	page.order    = 5
+	page.i18n     = "public"
+	page.index    = true
+
 	page          = node("public","status")
 	page.title    = _("Status")
 	page.target   = alias("public","status","olsr")
@@ -14,6 +21,14 @@ function index()
 	page.setgroup = "nogroup"
 	page.i18n     = "Status"
 	page.index    = true
-	assign({"public","status","olsr"}, {"admin", "status", "olsr"}, _("OLSR1"), 40)
-	assign({"freifunk","olsr","neighbors"}, {"admin", "status", "olsr","neighbors"}, _("OLSR1 neighbors"), 40)
+
+	page          = assign({"public","status","olsr"}, {"admin", "status", "olsr"}, _("OLSR1"), 40)
+	page.setuser  = "nobody"
+	page.setgroup = "nogroup"
+	page.acl_depends = {}
+
+	page          = assign({"freifunk","olsr","neighbors"}, {"admin", "status", "olsr","neighbors"}, _("OLSR1 neighbors"), 40)
+	page.setuser  = "nobody"
+	page.setgroup = "nogroup"
+	page.acl_depends = {}
 end
