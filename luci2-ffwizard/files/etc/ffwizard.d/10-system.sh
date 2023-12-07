75cbf738981e0b6951cb0664da7db52b42ad0a32
Patrick Grimm <patrick@lunatiki.de>
2023-12-07 18:18:34 +0100

    luci2-ffwizard: cleanup and rename luci2-ffwizard-lua to luci-app-ffwizard no lua deps

--- luci2-ffwizard/files/etc/ffwizard.d/10-system.sh
+++ luci2-ffwizard/files/etc/ffwizard.d/10-system.sh
@@ -1,8 +1,3 @@
-#!/bin/sh
-# Copyright (C) 2016 OpenWrt.org
-
-. /lib/functions.sh
-. /usr/share/libubox/jshn.sh
 
 log_system() {
 	logger -s -t ffwizard_system $@
@@ -44,34 +39,16 @@ setup_system() {
 }
 
 #Load ffwizard config
-json_init
-json_load "$(cat /tmp/config.json)"
-if ! json_select router ; then
-	log_system "Exit no valid json"
-	return 1
-fi
+config_load ffwizard
+
 # Set Hostname
-json_get_var hostname name "OpenWrt"
+config_get hostname ffwizard hostname "OpenWrt"
 
-json_select sshkeys
-json_select keys
-i=1;while json_is_a ${i} string;do
-	json_get_var key ${i}
-	echo $key >> /etc/dropbear/authorized_keys
-	i=$(( i + 1 ))
-done
+# Set lat lon
+config_get location ffwizard location
+config_get latitude ffwizard latitude
+config_get longitude ffwizard longitude
 
-json_select ..
-json_select ..
-json_select ..
-json_select location
-json_get_var latitude lat
-json_get_var longitude lng
-json_get_var street street
-json_get_var postalCode postalCode
-json_get_var city city
-
-local location="$street $postalCode $city"
 
 #Load dhcp config
 config_load system
--- luci2-ffwizard/files/www/luci-ng/proto/batadv.js
+++
@@ -1,8 +0,0 @@
-L2.invoke(['l2network', 'gettext', function(l2network, gettext) {
-	l2network.registerProtocolHandler({
-		protocol:    'batadv',
-		description: gettext('B.A.T.M.A.N. Advanced'),
-		tunnel:      false,
-		virtual:     true
-	});
-}]);
