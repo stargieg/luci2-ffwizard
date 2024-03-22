92a749c58ec30d7cff534209508b5da930f1b7bd
Patrick Grimm <patrick@lunatiki.de>
2024-03-22 20:51:14 +0100

    luci-app-olsrd2: FIX source_prefix datatype

--- luci-app-olsrd2/htdocs/luci-static/resources/view/olsrd2/olsrv2_lan.js
+++ luci-app-olsrd2/htdocs/luci-static/resources/view/olsrd2/olsrv2_lan.js
@@ -14,7 +14,7 @@ return view.extend({
 		o = s.option(form.Value, "name", _("Name"), "Text");
 		o.datatype = "string";
 		o = s.option(form.Value, "prefix", _("locally attached network prefix"), "");
-		o.datatype = "string";
+		o.datatype = "cidr6";
 		o = s.option(form.Value, "domain", _("domain for this LAN entry, -1 for all domains"), "-1-254");
 		o.optional = true;
 		o.placeholder = -1;
@@ -23,9 +23,9 @@ return view.extend({
 		o.optional = true;
 		o.placeholder = 2;
 		o.datatype = "range(0,254)";
-		o = s.option(form.Flag, "source_prefix", _("source prefix for lan (source specific routing)"), "");
+		o = s.option(form.Value, "source_prefix", _("source prefix for lan (source specific routing)"), "");
 		o.optional = true;
-		o.datatype = "bool";
+		o.datatype = "cidr6";
 
 		return m.render();
 	}
