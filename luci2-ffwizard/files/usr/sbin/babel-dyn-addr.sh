c579459c2e9544b0f0d4fd3a21d6e8b09490746f
Patrick Grimm <patrick@lunatiki.de>
2025-01-04 01:25:52 +0100

    ffwizard: babel dyn addr use owipcalc

--- luci2-ffwizard/files/usr/sbin/babel-dyn-addr.sh
+++ luci2-ffwizard/files/usr/sbin/babel-dyn-addr.sh
@@ -6,157 +6,6 @@ log() {
 	logger -t babel-dyn-addr $@
 }
 
-calc_from_60() {
-	addr=$1
-	OIFS=$IFS
-	IFS=:
-	set $addr
-	IFS=$OIFS
-	echo -n $1:
-	if [ -z $2 ] ; then
-		echo -n 0: 
-	else
-		echo -n $2:
-	fi
-	if [ -z $3 ] ; then
-		echo -n 0:
-	else
-		echo -n $3:
-	fi
-	#mask=64
-	rand1="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-	if [ -z $4 ] ; then
-		[ "$rand1" == "0" ] && rand1=""
-		echo -n $rand1:
-	else
-		echo -n $(echo $4 | sed -e "s/0$/$rand1/"):
-	fi
-	echo :
-}
-
-calc_from_56() {
-	addr=$1
-	mask=$2
-	OIFS=$IFS
-	IFS=:
-	set $addr
-	IFS=$OIFS
-	echo -n $1:
-	if [ -z $2 ] ; then
-		echo -n 0:
-	else
-		echo -n $2:
-	fi
-	if [ -z $3 ] ; then
-		echo -n 0:
-	else
-		echo -n $3:
-	fi
-	rand1="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-	rand2=""
-	#die letzten stellen eines 62 netzes können nur 0,4,8,c sein.
-	if [ "$mask" == "62" ] ; then
-		while [ "$rand2" != "0" ] && [ "$rand2" != "4" ] && [ "$rand2" != "8" ] && [ "$rand2" != "c" ] ; do
-			rand2="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-		done
-	else #mask=64
-		rand2="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-	fi
-
-	if [ -z $4 ] ; then
-		[ "$rand1" == "0" ] && rand1=""
-		[ -z "$rand1" ] && [ "$rand2" == "0" ] && rand2=""
-		echo -n $rand1$rand2:
-	else
-		echo -n $(echo $4 | sed -e "s/00$/$rand1$rand2/"):
-	fi
-	echo :
-}
-
-calc_from_52() {
-	addr=$1
-	mask=$2
-	OIFS=$IFS
-	IFS=:
-	set $addr
-	IFS=$OIFS
-	echo -n $1:
-	if [ -z $2 ] ; then
-		echo -n 0:
-	else
-		echo -n $2:
-	fi
-	if [ -z $3 ] ; then
-		echo -n 0:
-	else
-		echo -n $3:
-	fi
-	rand1="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-	rand2="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-	rand3=""
-	if [ "$mask" == "62" ] ; then
-		while [ "$rand3" != "0" ] && [ "$rand3" != "4" ] && [ "$rand3" != "8" ] && [ "$rand3" != "c" ] ; do
-			rand3="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-		done
-	else #mask=64
-		rand3="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-	fi
-
-	if [ -z $4 ] ; then
-		[ "$rand1" == "0" ] && rand1=""
-		[ -z "$rand1" ] && [ "$rand2" == "0" ] && rand2=""
-		[ -z "$rand2" ] && [ "$rand3" == "0" ] && rand3=""
-		echo -n $rand1$rand2$rand3:
-	else
-		echo -n $(echo $4 | sed -e "s/000$/$rand1$rand2$rand3/"):
-	fi
-	echo :
-}
-
-calc_from_48() {
-	addr=$1
-	mask=$2
-	OIFS=$IFS
-	IFS=:
-	set $addr
-	IFS=$OIFS
-	echo -n $1:
-	if [ -z $2 ] ; then
-		echo -n 0:
-	else
-		echo -n $2:
-	fi
-	rand1="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-	rand2="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-	rand3="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-	rand4=""
-	if [ "$mask" == "62" ] ; then
-		while [ "$rand4" != "0" ] && [ "$rand4" != "4" ] && [ "$rand4" != "8" ] && [ "$rand4" != "c" ] ; do
-			rand4="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-		done
-	else #mask=64
-		rand4="$(head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1)"
-	fi
-	[ "$rand1" == "0" ] && rand1=""
-	[ -z "$rand1" ] && [ "$rand2" == "0" ] && rand2=""
-	[ -z "$rand2" ] && [ "$rand3" == "0" ] && rand3=""
-	[ -z "$rand3" ] && [ "$rand4" == "0" ] && rand4=""
-	if [ -z $3 ] ; then
-		if [ -z $rand4 ] ; then
-			echo -n :
-		else
-			echo -n 0:$rand1$rand2$rand3$rand4:
-		fi
-	else
-		if [ -z $rand4 ] ; then
-			echo -n $3:
-		else
-			echo -n $3:$rand1$rand2$rand3$rand4:
-		fi
-	fi
-	echo :
-}
-
 chk_red() {
 	local cfg=$1
 	config_get ip $cfg ip
@@ -187,6 +36,10 @@ ip6prefix_new=""
 prefixs=""
 metric=65535
 genmask=128
+uci_mask="$(uci_get ffwizard ffwizard ip6mask 64)"
+uci_ffprefix=$(uci_get network fflandhcp ffprefix)
+uci_ip6prefix=$(uci_get network fflandhcp ip6prefix)
+
 ubus call babeld get_routes | \
 jsonfilter -e '@.IPv6[@.address="::/0"]' > /tmp/babel-dyn-addr.json
 while read line; do
@@ -194,100 +47,72 @@ while read line; do
 		-e 'installed=@.installed' \
 		-e 'src_prefix=@.src_prefix' \
 		-e 'refmetric=@.refmetric' )
-	if [ "$installed" == "1" ] ; then
-		src_mask="$(echo $src_prefix | cut -d '/' -f 2)"
-		if [ $src_mask -eq 48 ] || [ $src_mask -eq 52 ] || [ $src_mask -eq 56 ] || [ $src_mask -eq 60 ]; then
-			if [ $refmetric -le $metric ] ; then
-				prefixs="$src_prefix $prefixs"
-				metric=$refmetric
-				#genmask=$src_mask
-			else
-				prefixs="$prefixs $src_prefix"
-				metric=$refmetric
-			fi
-		fi
+	[ "$installed" == "1" ] || continue
+	if [ $refmetric -le $metric ] ; then
+		prefixs="$src_prefix $prefixs"
+		metric=$refmetric
+	else
+		prefixs="$prefixs $src_prefix"
+		metric=$refmetric
 	fi
 done < /tmp/babel-dyn-addr.json
 rm /tmp/babel-dyn-addr.json
 
-if [ "$prefixs" == "" ] ; then
-	log "Exit no IPv6 neighbor entry"
-	return 1
-fi
-
 for prefix in $prefixs ; do
-	uciprefix=$(uci_get network fflandhcp ffprefix)
-	if [ "$prefix" == "$uciprefix" ] ; then
-		ip6prefix_new=$(uci_get network fflandhcp ip6prefix)
+	if [ "$prefix" == "$uci_ffprefix" ] ; then
+		ip6prefix_new="$uci_ip6prefix"
 	else
 		destination="$(echo $prefix | cut -d '/' -f 1)"
 		genmask="$(echo $prefix | cut -d '/' -f 2)"
-		if [ $genmask -eq 48 ] || [ $genmask -eq 52 ] || [ $genmask -eq 56 ] || [ $genmask -eq 60 ]; then
-			ula="$(echo $destination | cut -b -2)"
-			if [ ! "$destination" == "$srcip6prefix" ] ; then
-				log "new attached_net_src $attached_net_src"
-				case $genmask in
-				60)
-					srcip6prefix_new="$destination"
-					ip6prefix_new=$(calc_from_60 $destination)
-					ip6prefix_mask_new="64"
-					ip6prefix_new="$ip6prefix_new/$ip6prefix_mask_new"
-					;;
-				56)
-					srcip6prefix_new="$destination"
-					ip6prefix_new=$(calc_from_56 $destination 64)
-					ip6prefix_mask_new="64"
-					#ip6prefix_new=$(calc_from_56 $destination 62)
-					#ip6prefix_mask_new="62"
-					ip6prefix_new="$ip6prefix_new/$ip6prefix_mask_new"
-					;;
-				52)
-					srcip6prefix_new="$destination"
-					ip6prefix_new=$(calc_from_52 $destination 64)
-					ip6prefix_mask_new="64"
-					#ip6prefix_new=$(calc_from_56 $destination 62)
-					#ip6prefix_mask_new="62"
-					ip6prefix_new="$ip6prefix_new/$ip6prefix_mask_new"
-					;;
-				48)
-					srcip6prefix_new="$destination"
-					ip6prefix_new=$(calc_from_48 $destination 64)
-					ip6prefix_mask_new="64"
-					#ip6prefix_new=$(calc_from_56 $destination 62)
-					#ip6prefix_mask_new="62"
-					ip6prefix_new="$ip6prefix_new/$ip6prefix_mask_new"
-					;;
-				*)
-					log "wrong mask src $genmask"
-					;;
-				esac
-			else
-				log "wrong mask src $genmask"
-				ip6prefix_new=""
-			fi
-		fi
+		min_mask=$((uci_mask-4))
+		while [ $genmask -lt $min_mask ] ; do
+			genmask=$((genmask+3))
+			rand_offset=$(awk 'BEGIN {srand() ; print int(1 + rand() * 8)}')
+			while [ $rand_offset -gt 0 ] ; do
+				prefix=$(owipcalc $prefix next $genmask)
+				rand_offset=$((rand_offset-1))
+			done
+		done
+		src_mask_count=$(owipcalc $prefix howmany ::/$uci_mask)
+		rand_offset=$(awk 'BEGIN {srand() ; print int(1 + rand() * '$src_mask_count')}')
+		while [ $rand_offset -gt 0 ] ; do
+			prefix=$(owipcalc $prefix next $uci_mask)
+			rand_offset=$((rand_offset-1))
+		done
+		ip6prefix_new="$prefix"
 	fi
 
-	keys=$(ubus call babeld get_routes | \
-	jsonfilter -e '@.IPv6[@.src_prefix="::/0"].address')
-
 	valid="1"
-	for key in $keys ; do
-		if [ "$key" == "$ip6prefix_new" ] ; then
-			log "##############dublicated $ip6prefix_new ########################"
+	ubus call babeld get_routes | \
+	jsonfilter -e '@.IPv6[@.src_prefix="::/0"]' > /tmp/babel-dyn-addr.json
+	while read line; do
+		eval $(jsonfilter -s "$line" \
+			-e 'installed=@.installed' \
+			-e 'address=@.address' )
+		[ "$installed" == "1" ] || continue
+		invalid="0"
+		invalid=$(owipcalc $address contains $ip6prefix_new)
+		if [ "$invalid" == "1" ] ; then
+			log "###dublicated mesh prefix $address contains new prefix $ip6prefix_new ###"
 			valid="0"
 		fi
-	done
+		invalid=$(owipcalc $ip6prefix_new contains $address)
+		if [ "$invalid" == "1" ] ; then
+			log "###dublicated new prefix $ip6prefix_new contains mesh prefix $address ###"
+			valid="0"
+		fi
+	done < /tmp/babel-dyn-addr.json
+	rm /tmp/babel-dyn-addr.json
+
 	if [ $valid == 1 ] ; then
-		log "##############valid $ip6prefix_new ########################"
+		log "##############valid $ip6prefix_new ##################################"
 		break
 	fi
 done
 
 if [ "$valid" == "0" -o "$ip6prefix_new" == "" ] ; then
-	if [ ! "$uciprefix" == "" ] ; then
+	if [ ! "$uci_ffprefix" == "" ] ; then
 		uci_remove network fflandhcp ip6prefix 2>/dev/null
-		#uci_add_list network fflandhcp ip6class "local"
 		uci_remove network fflandhcp ffprefix 2>/dev/null
 		uci_set network fflandhcp ip6assign '64'
 		uci_commit network
@@ -299,15 +124,13 @@ if [ "$valid" == "0" -o "$ip6prefix_new" == "" ] ; then
 	else
 		log "no ip6prefix found"
 	fi
-elif [ ! "$prefix" == "$uciprefix" ] ; then
+elif [ ! "$prefix" == "$uci_ffprefix" ] ; then
 	ip6prefix="$ip6prefix_new"
 	uci_set network fflandhcp ip6prefix "$ip6prefix"
-	#uci_add_list network fflandhcp ip6class "local"
-	#uci_add_list network fflandhcp ip6class "fflandhcp"
 	uci_set network fflandhcp ffprefix "$prefix"
 	uci_set network fflandhcp ip6assign '64'
 	uci_commit network
-	log "reload network with new $ip6prefix and old $uciprefix"
+	log "reload network with new $ip6prefix and old $uci_ffprefix"
 	ubus call rc init '{"name":"network","action":"reload"}' 2>/dev/null || /etc/init.d/network reload
 	sleep 3
 	ubus call rc init '{"name":"odhcpd","action":"restart"}' 2>/dev/null || /etc/init.d/odhcpd restart
