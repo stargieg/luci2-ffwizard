bab8c061f9c3ec15fcbac9d48a2a34c51b74fc16
Patrick Grimm <patrick@lunatiki.de>
2025-01-07 20:12:32 +0100

    ffwizard: add exclude and pref for auto prefix

--- luci2-ffwizard/files/usr/sbin/babel-dyn-addr.sh
+++ luci2-ffwizard/files/usr/sbin/babel-dyn-addr.sh
@@ -6,6 +6,24 @@ log() {
 	logger -t babel-dyn-addr $@
 }
 
+uci_add_list() {
+	local PACKAGE="$1"
+	local CONFIG="$2"
+	local OPTION="$3"
+	local VALUE="$4"
+
+	/sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} add_list "$PACKAGE.$CONFIG.$OPTION=$VALUE"
+}
+
+uci_add_list_state() {
+	local PACKAGE="$1"
+	local CONFIG="$2"
+	local OPTION="$3"
+	local VALUE="$4"
+
+	/sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} -P /var/state add_list "$PACKAGE.$CONFIG.$OPTION=$VALUE"
+}
+
 chk_red() {
 	local cfg=$1
 	config_get ip $cfg ip
@@ -13,10 +31,112 @@ chk_red() {
 	[ "$ip" == "::/0" ] && exit 1
 }
 
+chk_dublicated() {
+	local ip6prefix="$1"
+	local is_valid="1"
+	[ -z "$ip6prefix" ] && return
+	#log "chk_dublicated $ip6prefix"
+	ubus call babeld get_routes | \
+	jsonfilter -e '@.IPv6[@.src_prefix="::/0"]' > /tmp/babel-dyn-addr.json
+	while read line; do
+		eval $(jsonfilter -s "$line" \
+			-e 'installed=@.installed' \
+			-e 'route_metric=@.route_metric' \
+			-e 'address=@.address' )
+		[ "$installed" == "0" ] && continue
+		[ "$route_metric" -ge "16383" ] && continue
+		local invalid
+		invalid=$(owipcalc $address contains $ip6prefix)
+		if [ "$invalid" == "1" ] ; then
+			#log "###dublicated mesh prefix $address contains new prefix $ip6prefix ###"
+			is_valid="0"
+		fi
+		invalid=$(owipcalc $ip6prefix contains $address)
+		if [ "$invalid" == "1" ] ; then
+			#log "###dublicated new prefix $ip6prefix contains mesh prefix $address ###"
+			is_valid="0"
+		fi
+	done < /tmp/babel-dyn-addr.json
+	rm /tmp/babel-dyn-addr.json
+	if [ "$is_valid" == "1" ] ; then
+		echo "$ip6prefix"
+	else
+		echo ""
+	fi
+}
+
+get_rand_addr() {
+	local src_prefix="$1"
+	local mask="$2"
+	local destination="$(echo $src_prefix | cut -d '/' -f 1)"
+	local src_mask="$(echo $src_prefix | cut -d '/' -f 2)"
+	if [ $mask -lt $src_mask ] ; then
+		return
+	fi
+	if [ $mask -eq $src_mask ] ; then
+		echo "$src_prefix"
+		return
+	fi
+	min_mask=$((mask-4))
+	while [ $src_mask -lt $min_mask ] ; do
+		src_mask=$((src_mask+3))
+		#compat openwrt 19 doesn't support $RANDOM
+		#rand_offset=$(awk -v seed=$RANDOM 'BEGIN {srand(seed) ; print int(rand() * 9)}')
+		rand_offset=$(awk 'BEGIN {srand() ; print int(rand() * 9)}')
+		#log "rand_offset: $rand_offset"
+		while [ $rand_offset -gt 0 ] ; do
+			src_prefix=$(owipcalc $src_prefix next $src_mask)
+			rand_offset=$((rand_offset-1))
+		done
+		src_prefix=$(owipcalc $src_prefix prefix $src_mask)
+	done
+	if [ $mask -eq $src_mask ] ; then
+		echo "$src_prefix"
+		return
+	fi
+	#log "owipcalc $src_prefix howmany ::/$mask"
+	src_mask_count=$(owipcalc $src_prefix howmany ::/$mask)
+	#log "src_mask_count: $src_mask_count"
+	#rand_offset=$(awk -v seed=$RANDOM 'BEGIN {srand(seed) ; print int(rand() * '$src_mask_count')}')
+	rand_offset=$(awk 'BEGIN {srand() ; print int(rand() * '$src_mask_count')}')
+	#log "rand_offset: $rand_offset"
+	while [ $rand_offset -gt 0 ] ; do
+		src_prefix=$(owipcalc $src_prefix next $mask)
+		rand_offset=$((rand_offset-1))
+	done
+	src_prefix=$(owipcalc $src_prefix prefix $mask)
+	echo "$src_prefix"
+}
+
+set_new_prefix() {
+	local prefix="$1"
+	local ffprefix="$2"
+	log "reload network with new $prefix from $ffprefix"
+	uci_set network fflandhcp ip6prefix "$prefix"
+	uci_set network fflandhcp ffprefix "$ffprefix"
+	uci_commit network
+	ubus call rc init '{"name":"network","action":"reload"}' 2>/dev/null || /etc/init.d/network reload
+	sleep 3
+	ubus call rc init '{"name":"odhcpd","action":"restart"}' 2>/dev/null || /etc/init.d/odhcpd restart
+	ubus call rc init '{"name":"dnsmasq","action":"restart"}' 2>/dev/null || /etc/init.d/dnsmasq restart
+}
+
+remove_prefix() {
+	local prefix="$1"
+	log "remove uci prefix $prefix"
+	uci_remove network fflandhcp ip6prefix 2>/dev/null
+	uci_remove network fflandhcp ffprefix 2>/dev/null
+	uci_commit network
+	ubus call rc init '{"name":"network","action":"reload"}' 2>/dev/null || /etc/init.d/network reload
+	sleep 3
+	ubus call rc init '{"name":"odhcpd","action":"restart"}' 2>/dev/null || /etc/init.d/odhcpd restart
+	ubus call rc init '{"name":"dnsmasq","action":"restart"}' 2>/dev/null || /etc/init.d/dnsmasq restart
+}
+
 config_load babeld
 config_foreach chk_red filter
 
-if uci_get ffwizard ffwizard ip6prefix ; then
+if uci_get ffwizard ffwizard ip6prefix > /dev/null; then
 	log "prefix set by ffwizard"
 	return 1
 fi
@@ -36,10 +156,20 @@ ip6prefix_new=""
 prefixs=""
 metric=65535
 genmask=128
-uci_mask="$(uci_get ffwizard ffwizard ip6mask 64)"
+uci_mask="$(uci_get ffwizard autoconf ip6mask 64)"
+uci_prefix_pref=$(uci_get ffwizard autoconf prefix_pref)
+uci_prefix_exclude=$(uci_get ffwizard autoconf prefix_exclude)
 uci_ffprefix=$(uci_get network fflandhcp ffprefix)
 uci_ip6prefix=$(uci_get network fflandhcp ip6prefix)
+uci_ip6prefix_mask="$(echo $uci_ip6prefix | cut -d '/' -f 2)"
+if [ $uci_ip6prefix_mask != $uci_mask ] ; then
+	uci_ffprefix=""
+fi
 
+#howto uci.load /var/state
+#https://openwrt.github.io/luci/jsapi/LuCI.uci.html#load
+#uci_revert_state ffwizard autoconf prefix_available
+uci_remove ffwizard autoconf prefix_available
 ubus call babeld get_routes | \
 jsonfilter -e '@.IPv6[@.address="::/0"]' > /tmp/babel-dyn-addr.json
 while read line; do
@@ -48,6 +178,12 @@ while read line; do
 		-e 'src_prefix=@.src_prefix' \
 		-e 'refmetric=@.refmetric' )
 	[ "$installed" == "1" ] || continue
+	#uci_add_list_state ffwizard autoconf prefix_available "$src_prefix"
+	uci_add_list ffwizard autoconf prefix_available "$src_prefix"
+	[ "$src_prefix" == "$uci_prefix_exclude" ] && continue
+	if [ "$src_prefix" == "$uci_prefix_pref" ] ; then
+		refmetric=0
+	fi
 	if [ $refmetric -le $metric ] ; then
 		prefixs="$src_prefix $prefixs"
 		metric=$refmetric
@@ -57,86 +193,55 @@ while read line; do
 	fi
 done < /tmp/babel-dyn-addr.json
 rm /tmp/babel-dyn-addr.json
+uci_commit ffwizard
 
-new="0"
-valid="1"
 for prefix in $prefixs ; do
-	ffprefix_new="$prefix"
+	ffprefix_new=""
 	if [ "$prefix" == "$uci_ffprefix" ] ; then
-		new="0"
-		ip6prefix_new="$uci_ip6prefix"
+		#log "found uci ip6prefix $uci_ffprefix $uci_ip6prefix"
+		ip6prefix_new=$(chk_dublicated $uci_ip6prefix)
+		#retry 1
+		if [ -z "$ip6prefix_new" ] ; then
+			#log "retry 1 $prefix"
+			ip6prefix_new=$(get_rand_addr $prefix $uci_mask)
+			ip6prefix_new=$(chk_dublicated $ip6prefix_new)
+		fi
+		#retry 2
+		if [ -z "$ip6prefix_new" ] ; then
+			#log "retry 2 $prefix"
+			ip6prefix_new=$(get_rand_addr $prefix $uci_mask)
+			ip6prefix_new=$(chk_dublicated $ip6prefix_new)
+		fi
+		if [ -z "$ip6prefix_new" ] ; then
+			#log "remove uci ip6prefix $uci_ffprefix $uci_ip6prefix"
+			remove_prefix "$uci_ip6prefix"
+		else
+			exit
+		fi
 	else
-		new="1"
-		destination="$(echo $prefix | cut -d '/' -f 1)"
-		genmask="$(echo $prefix | cut -d '/' -f 2)"
-		min_mask=$((uci_mask-4))
-		while [ $genmask -lt $min_mask ] ; do
-			genmask=$((genmask+3))
-			rand_offset=$(awk 'BEGIN {srand() ; print int(1 + rand() * 8)}')
-			while [ $rand_offset -gt 0 ] ; do
-				prefix=$(owipcalc $prefix next $genmask)
-				rand_offset=$((rand_offset-1))
-			done
-		done
-		src_mask_count=$(owipcalc $prefix howmany ::/$uci_mask)
-		rand_offset=$(awk 'BEGIN {srand() ; print int(1 + rand() * '$src_mask_count')}')
-		while [ $rand_offset -gt 0 ] ; do
-			prefix=$(owipcalc $prefix next $uci_mask)
-			rand_offset=$((rand_offset-1))
-		done
-		ip6prefix_new="$prefix"
+		#log "try prefix $prefix"
+		ip6prefix_new=$(get_rand_addr $prefix $uci_mask)
+		ip6prefix_new=$(chk_dublicated $ip6prefix_new)
+		#retry 1
+		if [ -z "$ip6prefix_new" ] ; then
+			#log "retry 1 $prefix"
+			ip6prefix_new=$(get_rand_addr $prefix $uci_mask)
+			ip6prefix_new=$(chk_dublicated $ip6prefix_new)
+		fi
+		#retry 2
+		if [ -z "$ip6prefix_new" ] ; then
+			#log "retry 2 $prefix"
+			ip6prefix_new=$(get_rand_addr $prefix $uci_mask)
+			ip6prefix_new=$(chk_dublicated $ip6prefix_new)
+		fi
+		if [ -n "$ip6prefix_new" ] ; then
+			set_new_prefix "$ip6prefix_new" "$prefix"
+			exit
+		fi
 	fi
 
-	valid="1"
-	ubus call babeld get_routes | \
-	jsonfilter -e '@.IPv6[@.src_prefix="::/0"]' > /tmp/babel-dyn-addr.json
-	while read line; do
-		eval $(jsonfilter -s "$line" \
-			-e 'installed=@.installed' \
-			-e 'address=@.address' )
-		[ "$installed" == "1" ] || continue
-		invalid="0"
-		invalid=$(owipcalc $address contains $ip6prefix_new)
-		if [ "$invalid" == "1" ] ; then
-			log "###dublicated mesh prefix $address contains new prefix $ip6prefix_new ###"
-			valid="0"
-		fi
-		invalid=$(owipcalc $ip6prefix_new contains $address)
-		if [ "$invalid" == "1" ] ; then
-			log "###dublicated new prefix $ip6prefix_new contains mesh prefix $address ###"
-			valid="0"
-		fi
-	done < /tmp/babel-dyn-addr.json
-	rm /tmp/babel-dyn-addr.json
-
-	if [ $valid == 1 ] ; then
-		log "##############valid $ip6prefix_new ##################################"
-		break
-	fi
 done
 
-if [ "$valid" == "0" -o "$ip6prefix_new" == "" ] ; then
-	if [ ! "$uci_ffprefix" == "" ] ; then
-		uci_remove network fflandhcp ip6prefix 2>/dev/null
-		uci_remove network fflandhcp ffprefix 2>/dev/null
-		uci_set network fflandhcp ip6assign '64'
-		uci_commit network
-		log "reload network with no ip6prefix"
-		ubus call rc init '{"name":"network","action":"reload"}' 2>/dev/null || /etc/init.d/network reload
-		sleep 3
-		ubus call rc init '{"name":"odhcpd","action":"restart"}' 2>/dev/null || /etc/init.d/odhcpd restart
-		ubus call rc init '{"name":"dnsmasq","action":"restart"}' 2>/dev/null || /etc/init.d/dnsmasq restart
-	else
-		log "no ip6prefix found"
-	fi
-elif [ "$new" == "1" ] ; then
-	uci_set network fflandhcp ip6prefix "$ip6prefix_new"
-	uci_set network fflandhcp ffprefix "$ffprefix_new"
-	uci_set network fflandhcp ip6assign '64'
-	uci_commit network
-	log "reload network with new $ip6prefix_new and old $uci_ffprefix"
-	ubus call rc init '{"name":"network","action":"reload"}' 2>/dev/null || /etc/init.d/network reload
-	sleep 3
-	ubus call rc init '{"name":"odhcpd","action":"restart"}' 2>/dev/null || /etc/init.d/odhcpd restart
-	ubus call rc init '{"name":"dnsmasq","action":"restart"}' 2>/dev/null || /etc/init.d/dnsmasq restart
+if [ -n "$uci_ip6prefix" ] ; then
+	remove_prefix "$uci_ip6prefix"
 fi
--- luci2-ffwizard/files/www/luci-static/resources/view/ffwizard/autoconf.js
+++ luci2-ffwizard/files/www/luci-static/resources/view/ffwizard/autoconf.js
@@ -1,19 +1,54 @@
 'use strict';
+'require uci';
 'require view';
 'require form';
 
 return view.extend({
+	load: function() {
+		return Promise.all([
+			uci.load('ffwizard'),
+			uci.load('network')
+		]);
+	},
 	render: function() {
 		var m, s, o;
+		var prefix_available = uci.get('ffwizard', 'autoconf', 'prefix_available') || {};
+		var prefix_selected = uci.get('network', 'fflandhcp', 'ffprefix') || '';
+		var prefix_rand = uci.get('network', 'fflandhcp', 'ip6prefix') || '';
 
 		m = new form.Map('ffwizard', 'Autokonfiguration');
 
 		s = m.section(form.TypedSection, 'autoconf', _('Autokonfiguration'));
 		s.anonymous = false;
 		s.addremove = false;
+
 		o = s.option(form.Flag, "firstboot", _("Start der autokonfiguration"), "");
 		o.datatype = "bool";
 		o.rmempty = false;
+
+		o = s.option(form.Value, "prefix_pref", _("Bevorzugtes IPv6 Prefix."), "2001:bf7::/32");
+		o.datatype = "cidr6";
+		o.optional = true;
+		for (var i = 0; i < prefix_available.length; i++) 
+		{
+			o.value(prefix_available[i]);
+		}
+
+		o = s.option(form.Value, "ip6mask", _("IPv6 Netzgröße"), "1-64");
+		o.placeholder = "64";
+		o.datatype = "range(0,128)";
+
+		o = s.option(form.Value, "prefix_exclude", _("Ausgeschlossenes IPv6 Prefix."), "2001:bf7::/32");
+		o.datatype = "cidr6";
+		o.optional = true;
+		for (var i = 0; i < prefix_available.length; i++) 
+		{
+			o.value(prefix_available[i]);
+		}
+		o = s.option(form.DummyValue, "", _("Ausgewähltes IPv6 Prefix."));
+		o.cfgvalue = function () {return prefix_selected};
+		o = s.option(form.DummyValue, "", _("Gewürfeltes IPv6 Prefix."));
+		o.cfgvalue = function () {return prefix_rand};
 
 		return m.render();
 	}
