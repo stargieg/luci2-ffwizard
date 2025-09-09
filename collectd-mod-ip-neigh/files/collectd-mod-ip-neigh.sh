6812e497979ea883f52beaa6ef7a7282cf60330d
Patrick Grimm <patrick@lunatiki.de>
2025-09-09 19:45:49 +0200

    collectd-mod-ip-neigh: cleanup

--- collectd-mod-ip-neigh/files/collectd-mod-ip-neigh.sh
+++ collectd-mod-ip-neigh/files/collectd-mod-ip-neigh.sh
@@ -24,50 +24,10 @@ function read_dhcpleases() {
 	echo "PUTVAL \"$ident\" interval=$INTERVAL N:$ret_sum"
 }
 
-function read_neigh() {
-	local plugin=dhcpleases
-	#local plugin=neigh
-	local plugin_instance="$1"
-	local type="count"
-	local type_instance="$2"
-	local iface="$3"
 
-	local ident="$HOSTNAME/$plugin-$plugin_instance/$type"
-
-	local ret
-
-	case $type_instance in
-	combined)
-		ident="$ident-${type_instance}"
-		ret=$(ip neigh show dev "$iface" | grep REACHABLE | cut -d ' ' -f 3 | sort | uniq | wc -w)
-		;;
-	ipv4)
-		ident="$ident-${type_instance}"
-		ret=$(ip -4 neigh show dev "$iface" | grep REACHABLE | cut -d ' ' -f 3 | sort | uniq | wc -w)
-		;;
-	ipv6)
-		ident="$ident-${type_instance}"
-		ret=$(ip -6 neigh show dev "$iface" | grep REACHABLE | cut -d ' ' -f 3 | sort | uniq | wc -w)
-		;;
-	esac
-	echo "PUTVAL \"$ident\" interval=$INTERVAL N:$ret"
-
-}
-
-
-
-while true
+while sleep "$INTERVAL"
 do
 	read_dhcpleases "$iface_list"
-
-	# for iface in $iface_list ; do
-	# 	iface_l2="$iface"
-	# 	read_neigh "$iface" "combined" "$iface_l2"
-	# 	read_neigh "$iface" "ipv4" "$iface_l2"
-	# 	read_neigh "$iface" "ipv6" "$iface_l2"
-	# done
-
-	sleep "$INTERVAL"
 done
 
 exit 0
